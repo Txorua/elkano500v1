90c5eee3218d60961d9df623e97eabae21a157cf
Iñaki García Blasco <txorua@txorua.com>
2014-11-05 11:44:46 +0100

    Rediseño la portada según prototipo nuevo

---
+++ templates/views-view-unformatted--portada--page-1.tpl.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php if (!empty($title)): ?>
+  <h3><?php print $title; ?></h3>
+<?php endif; ?>
+
+<?php foreach ($rows as $id => $row): ?>
+
+  <?php ($classes_array[$id] && $id < 2) ? $classes_array[$id] .= " col-md-6" : $classes_array[$id] .= " col-md-4"; ?>
+
+  <?php if ($id == 0 || $id == 2): ?>
+  <div class="row">
+  <?php endif; ?>
+
+  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
+    <?php print $row; ?>
+  </div>
+
+  <?php if ($id == 1): ?>
+  </div>
+  <?php endif; ?>
+
+<?php endforeach; ?>
+</div>
